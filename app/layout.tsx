dd870b8ff0ee3bc0013cbc137d3e935078518a71
Roman Zhurakovskiy <rzhurakovskiy.webdev@yandex.ru>
2024-09-16 01:01:18 +0300

    add yandex metrika

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import localFont from "next/font/local";
 import "./css/reset.css";
 import "./css/main.css";
 import "./css/media.css";
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 import Preloader from "./components/Preloader";
 import Head from "next/head";
@@ -50,8 +50,39 @@ export default function RootLayout({
             "url": "https://romanzhurakovskiy.vercel.app/",
           }`}
         </script>
+
+        {/* Yandex.Metrika */}
+        <script type="text/javascript">
+          {`
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+              ym(98345458, "init", {
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true
+              });
+          `}
+        </script>
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/98345458"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
+        {/* /Yandex.Metrika */}
       </Head>
-      <body suppressHydrationWarning={true} className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body
+        suppressHydrationWarning={true}
+        className={`${geistSans.variable} ${geistMono.variable}`}
+      >
         <Preloader />
         <ToastContainer />
         {children}
