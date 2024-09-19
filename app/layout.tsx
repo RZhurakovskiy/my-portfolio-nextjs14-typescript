ab9aa4003d7f94dbeef7ac584ddf1537b2c4a85e
RZhurakovskiy <130534672+RZhurakovskiy@users.noreply.github.com>
2024-09-19 09:04:03 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,10 +5,8 @@ import "./css/main.css";
 import "./css/media.css";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
-
 import Preloader from "./components/Preloader";
 import Head from "next/head";
-
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -19,7 +17,6 @@ const geistMono = localFont({
   variable: "--font-geist-mono",
   weight: "100 900",
 });
-
 export const metadata: Metadata = {
   title: "Roman Zhurakovskiy portfolio",
   description: "Roman Zhurakovskiy by create next app",
@@ -30,13 +27,12 @@ export const metadata: Metadata = {
     type: "website",
   },
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
+   return (
     <html lang="ru">
       <Head>
         <meta charSet="UTF-8" />
@@ -50,17 +46,42 @@ export default function RootLayout({
             "url": "https://romanzhurakovskiy.vercel.app/"
           }`}
         </script>
-
-      
+        {/* Yandex.Metrika */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+              ym(98345458, "init", {
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true
+              });
+          `,
+          }}
+        ></script>
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/98345458"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
+        {/* /Yandex.Metrika */}
       </Head>
       <body
         suppressHydrationWarning={true}
-        className={${geistSans.variable} ${geistMono.variable}}
+        className={`${geistSans.variable} ${geistMono.variable}`}
       >
         <Preloader />
         <ToastContainer />
-        {children}
-      </body>
+         {children}
+              </body>
     </html>
   );
-}
