4b0c5a014cfc9f52e5c6fea7722ecc030bb19b45
RZhurakovskiy <130534672+RZhurakovskiy@users.noreply.github.com>
2024-09-24 13:00:10 +0300

    Update About.tsx

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -3,7 +3,7 @@ const About: React.FC = () => {
   return (
     <section className="about-me" id="about-me">
       <div className="container">
-        <h2 className="portfolio-title">Обо мне</h2>
+        <h2 className="about-title">Обо мне</h2>
         <p className="portfolio-text">Стаж коммерческой веб-разработки — 2 года в крупном 
             российском производственном холдинге. Основное направление работы: разработка MES-систем и автоматизированных 
             систем управления в составе команды программистов.</p>
