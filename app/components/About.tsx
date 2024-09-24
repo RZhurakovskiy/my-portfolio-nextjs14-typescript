2c05fd5ff541be34aa4f9a2103ec806771ad03ba
RZhurakovskiy <130534672+RZhurakovskiy@users.noreply.github.com>
2024-09-24 12:50:55 +0300

    Create About.tsx section component

---
+++ app/components/About.tsx
@@ -0,0 +1,15 @@
+const About: React.FC = () => {
+ 
+  return (
+    <section className="about-me" id="about-me">
+      <div className="container">
+        <h2 className="portfolio-title">Портфолио</h2>
+        <p className="portfolio-text">Стаж коммерческой веб-разработки — 2 года в крупном 
+            российском производственном холдинге. Основное направление работы: разработка MES-систем и автоматизированных 
+            систем управления в составе команды программистов.</p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
